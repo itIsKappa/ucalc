528b9513ba9c0a9cfb9c36f28cc83bbdf3feed0c
Hathim Ali <honeyt2019@gmail.com>
2020-05-19 15:16:24 +0530

    Added install.sh for ease of installation [NEEDS ROOT]

---
+++ install.sh
@@ -0,0 +1,9 @@
+echo "Initializing installation..."
+echo "Moving ucalc to /usr/bin"
+mv ucalc /usr/bin
+echo "Creating ucalc directory in /usr/share"
+mkdir /usr/share/ucalc
+echo "Moving ucalcLog.txt to /usr/share/ucalc/"
+mv ucalcLog.txt /usr/share/ucalc/
+echo "###################### D O N E ######################"
+
