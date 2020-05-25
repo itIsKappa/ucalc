c21cf91519a7ca88127631841287447e02843b8e
Hathim Ali <honeyt2019@gmail.com>
2020-05-25 20:30:32 +0530

    Added creating COPYING in /usr/share/doc/ucalc/

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 echo "Initializing installation..."
 echo "Moving ucalc to /usr/bin"
 mv ucalc /usr/bin
+echo "[ OK ]"
 echo "Creating ucalc directory in /usr/share"
 mkdir /usr/share/ucalc
+echo "[ OK ]"
 echo "Moving ucalcLog.txt to /usr/share/ucalc/"
 mv ucalcLog.txt /usr/share/ucalc/
+echo "[ OK ]"
+echo "Creating copyright at /usr/share/doc/ucalc/"
+mkdir /usr/share/doc/ucalc
+mv COPYING /usr/share/doc/ucalc/
+echo "[ OK ]"
 echo "###################### D O N E ######################"
 
